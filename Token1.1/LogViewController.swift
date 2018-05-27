31012059e20e9a4e80f3fd500c7223a838f37c0e
Krindred <kualsop@gmail.com>
2018-05-27 13:58:15 -0400

    Connected picker view to log view controller

--- Token1.1/LogViewController.swift
+++ Token1.1/LogViewController.swift
@@ -8,6 +8,37 @@
 
 import UIKit
 
-class LogViewController: UIViewController {
+enum tags: Int {
+    case sober
+    case usedAcceptably
+    case used
+}
+
+class LogViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
+    @IBOutlet weak var PickerView: UIPickerView!
+    
+    var tags = [String]()
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return tags.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return tags[row]
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.PickerView.delegate = self
+        self.PickerView.dataSource = self
+        
+        self.tags = ["Sober", "Used", "Used Moderately"]
+        
+    }
 }
