42883bb893c4df71b7d785b8ea19f89a6b1005ba
Krindred <kualsop@gmail.com>
2018-05-27 13:52:25 -0400

    added log view controller with basic layout

---
+++ Token1.1/LogViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LogViewController.swift
+//  Token1.1
+//
+//  Created by Karin Alsop on 5/27/18.
+//  Copyright © 2018 Isabel. All rights reserved.
+//
+
+import UIKit
+
+class LogViewController: UIViewController {
+    
+}
